20d34a33519c0ecf79238af86bd1a15cd606ada8
jalpan07 <jalpan2808@gmail.com>
2025-11-23 16:39:02 +0530

    connected

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import CONNECT_DB from "./db/index.js";
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path:'./env'
+})
+// const app = express()
+// (async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`)
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is Listening on port ${process.env.PORT}`);
+            
+//         })
+//     }catch(e){
+//         console.log("ERROR: ",e); 
+//     }
+
+// })()
+
+CONNECT_DB()
